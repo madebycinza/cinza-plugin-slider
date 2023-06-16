4f1b58c18c2b31be466e14c906fa43be70fa313f
Vinicius Miazaki <vinicius@cinza.io>
2023-06-16 15:47:28 -0700

    v1.2.3

--- cinza-slider.php
+++ cinza-slider.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Cinza Slider
  * Plugin URI:        https://cinza.io/plugin/cinza-slider/
  * Description:       A minimal slider plugin.
- * Version:           1.2.2
+ * Version:           1.2.3
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Cinza Web Design
